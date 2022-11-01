80480a45e57f078af6843a9b414ca398b68573ee
Mistheil <57539079+Mistheil@users.noreply.github.com>
2022-11-01 19:55:28 +0700

    Upload

---
+++ src/learn-useState/Counter.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import logo from "../logo.svg";
+import "./Counter.css";
+export default function Counter() {
+    const [text, setText] = useState("");
+    const [showImage, setShowImage] = useState(true);
+    const [count, setCount] = useState(0);
+    const countUp = () => {
+        setCount(count + 1);
+    };
+    const countDown = () => {
+        setCount(count - 1);
+    };
+    const hideImage = () => {
+        setShowImage(!showImage);
+    };
+    return (
+        <div className="Main">
+            {/* <p className="Text">Counter with useState</p> //Counter with usestate */}
+            {/* <p className="Text">Dalam hati</p> */}
+            {/* <p>Dalamnya seberapa</p> */}
+            
+            <div className="ViewImage">
+                <div className="ViewImage1">
+                    <img
+                        className="App-logo"
+                        style={{ display: showImage === true ? "flex" : "none" }}
+                        src={logo} 
+                        alt="logo"
+                    />
+                </div>
+                <button className="Button" onClick={hideImage}>
+                    {showImage !== true ? "Tampilkan" : "Sembunyikan"}
+                </button>
+            </div>
+            <p> KELOMPOK 05</p>
+
+            <p className="Text">{count}</p>
+            <div className="ViewButton">
+                <div className="ViewButton2">
+                    <button className="Button" onClick={countUp}>
+                        Naik
+                    </button>
+                </div>
+                <div className="ViewButton1">
+                    <button className="Button" onClick={countDown}>
+                        Turun
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
